c4685d43770d0ea0535c1e856fbbafa892ff4704
Mystery0 <mystery0dyl@icloud.com>
2017-02-27 21:16:11 +0800

    整合iFloatMenu

---
+++ app/src/main/java/com/mystery0/toolsdemo/FloatMenuDemoActivity.java
@@ -0,0 +1,32 @@
+package com.mystery0.toolsdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.mystery0.ifloatmenu.MenuClick;
+import com.mystery0.ifloatmenu.iFloatMenu;
+
+public class FloatMenuDemoActivity extends AppCompatActivity
+{
+    private static final String TAG = "FloatMenuDemoActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_float_menu);
+
+        iFloatMenu iFloatMenu = (iFloatMenu) findViewById(R.id.ifloat);
+        iFloatMenu.setNumber(4);
+        iFloatMenu.setCloseIcon(R.drawable.ic_android);
+        iFloatMenu.setIcons(new int[]{R.drawable.ic_android, R.drawable.ic_android, R.drawable.ic_android, R.drawable.ic_android});
+        iFloatMenu.setMenuClickListener(new MenuClick()
+        {
+            @Override
+            public void menuClick(int position)
+            {
+                Log.i(TAG, "menuClick: " + position);
+            }
+        });
+    }
+}
